52396525cfe8edd812780b1560bb69b2c2228bfe
Gavin Michael <gavin.michael.ftw@gmail.com>
2019-03-10 09:00:43 +0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongoDBURI';
+dbPassword = 'mongodb+srv://dbUser:' + process.env.DBPW + '@cluster0-sixwu.mongodb.net/test?retryWrites=true';
 
 module.exports = {
     mongoURI: dbPassword
